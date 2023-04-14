5f054fe1ba3dc2844a16bec1def7f17d16f95f89
Hasni Saefi A <saefihasni@gmail.com>
2023-04-14 21:18:08 +0700

    add file sidebar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Sidebar from './components/Sidebar';
+import Dashboard from './pages/Dashboard';
+import About from './pages/About';
+import Comment from './pages/Comment';
+import Analytic from './pages/Analytic';
+import ProductList from './pages/ProductList';
+import Product from './pages/Product';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Sidebar>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/comment" element={<Comment />} />
+          <Route path="/analytics" element={<Analytic />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/productList" element={<ProductList />} />
+        </Routes>
+      </Sidebar>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Analytic.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Analytic = () => {
+  return (
+    <div>
+      <h1>Analytic pages</h1>
+    </div>
+  )
+}
+
+export default Analytic
